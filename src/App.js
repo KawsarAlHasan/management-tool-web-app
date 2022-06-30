be34fe87155482b377f838d966b21c13a8190549
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-06-30 23:04:09 +0600

    start management tool web app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <button class="btn btn-active btn-primary">Button</button>
+
+      <p>
+        Job Description: <br />
+        We are looking for a good front-end engineer. Please make a task management tool web app with the functionalities below. For example, the Google Tasks app, etc.
+        <br /> <br />
+        Task description: <br />
+        You will need to make a responsive navbar with at least these 03 routes. Completed Tasks, To-Do, Calendar. (You can add more routes on your own if you want.) <br /> <br />
+        There will be an input field, where users can add the daily tasks. After writing the tasks press the “enter” key on the keyboard to add the tasks to the To-Do route. Remember, To-Do lists have to be shown somewhere in the home route. (Look at the google task app for more understanding) <br /> <br />
+        Users can edit the tasks in the To-Do list section. You can use any clickable edit button to start the edit. <br /> <br />
+        There has to be a checkbox or radio symbol before the tasks. When the user clicks on that the tasks will go to the Completed tasks section. (Look at the google task app for more understanding) <br /> <br />
+        Use any calendar API to show the calendar in the Calendar route. (You can use any Design and Calendar API). <br /> <br />
+        Design a footer by your own.  <br /> <br />
+
+        Additional information: <br />
+        Use MERN (Mongo, Express, React, Node) stack to build this Web App. (What have you learned so far) <br />
+        Use your own design sense to earn bonus points. <br />
+        You can use any CSS framework, we recommend Tailwind CSS. <br />
+        Use your imagination to come up with something extra that’ll look good with the rest of the site. <br />
+        Your app must be responsive, use your imagination to make it responsive according to users' needs.
+
+      </p>
     </div>
   );
 }
